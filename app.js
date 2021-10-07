9fb9803e17f0d9bacca4124d95f12e0e22a60fb9
Samssoto1 <Samssoto1@gmail.com>
2021-10-07 08:30:07 -0800

    HerokuTest

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const PORT = process.env.PORT || 5000; // So we can run on heroku || (OR) localhost:5000
 const path = require('path');
-
+const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 5000; // So we can run on heroku || (OR) localhost:5000
 
 const errorController = require('./controllers/error');
+const User = require('./models/user');
 
 const app = express();
 
@@ -17,9 +19,52 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  User.findById('615d6062792c91fd2a8b1319')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+const corsOptions = {
+    origin: "https://cse341ecomsite.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
-app.listen(PORT);
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://Samssoto:onmyhonor@ecomnodeapp.dznww.mongodb.net/test";
+
+mongoose
+  .connect(
+    MONGODB_URL, options
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Sam',
+          email: 'Sam@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(PORT);
+  })
+  .catch(err => {
+    console.log(err);
+  });
